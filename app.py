b473d6d4dd252728c7ae665083e6b6fabc02a48e
Elias Müller <udjks@student.kit.edu>
2022-10-27 14:23:41 +0200

    Added: Personal labeling of the data, Navbar, some css, grid layout to html

--- app.py
+++ app.py
@@ -22,24 +22,83 @@ It works as follows:
 @app.route("/labeling")
 def labeling():
     # the current tweet
-    tweet = Climate.show_tweets()
-    return render_template("labeling.html", tweet = tweet)
+    tweet, sentiment, my_label = Climate.show_tweets()
+    return render_template("labeling.html", tweet = tweet, sentiment=sentiment, my_label=my_label)
 
 """
 This function gets triggered by the java script function called next_tweet
 """
 @app.route('/next_tweet')
-def SomeFunction():
+def next_tweet():
     print('next_tweet')
+    # point the indexer to the next tweet in the pandas df
     Climate.tweet_counter_climate += 1
-    next_tweet = Climate.show_tweets()
-    return render_template('labeling.html',next_tweet=next_tweet)
+    # show the next tweet
+    tweet, sentiment, my_label = Climate.show_tweets()
+    return render_template('labeling.html',tweet=tweet, sentiment=sentiment, my_label=my_label)
+
+"""
+This function gets triggered by the java script function called previous_tweet
+"""
+@app.route('/previous_tweet')
+def previous_tweet():
+    print('previous_tweet')
+    # point the indexer to the next tweet in the pandas df
+    Climate.tweet_counter_climate -= 1
+    # show the next tweet
+    tweet, sentiment, my_label = Climate.show_tweets()
+    return render_template('labeling.html',tweet=tweet, sentiment=sentiment, my_label=my_label)
+
+"""
+After the user labeled a tweet, the label will be put into the my_label
+column of the pandas DataFrame.
+Then the next tweet will automatically be show (discuss whether this 
+should be implemented or if the user should go to the next tweet by himself
+or even to make it a setting that the user can take)
+Currently we have four api calls for this --> make it less redundant by using best
+practices in the future
+"""
+
+@app.route("/manual_label_pro")
+def manual_label_pro():
+    Climate.label(1)
+    tweet, sentiment, my_label = Climate.show_tweets()
+    return render_template('labeling.html',tweet=tweet, sentiment=sentiment, my_label=my_label)
+
+@app.route("/manual_label_anti")
+def manual_label_anti():
+    Climate.label(-1)
+    tweet, sentiment, my_label = Climate.show_tweets()
+    return render_template('labeling.html',tweet=tweet, sentiment=sentiment, my_label=my_label)
+
+@app.route("/manual_label_neutral")
+def manual_label_neutral():
+    Climate.label(0)
+    tweet, sentiment, my_label = Climate.show_tweets()
+    return render_template('labeling.html',tweet=tweet, sentiment=sentiment, my_label=my_label)
+
+@app.route("/manual_label_news")
+def manual_label_news():
+    Climate.label(2)
+    tweet, sentiment, my_label = Climate.show_tweets()
+    return render_template('labeling.html',tweet=tweet, sentiment=sentiment, my_label=my_label)
+
+"""
+This page does ...
+"""
 
 @app.route("/analysis")
 def analysis():
     return render_template("analysis.html")
 
+"""
+This page does ...
+"""
 
 @app.route("/training")
 def training():
     return render_template("training.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- static/js/labeling.js
+++ static/js/labeling.js
@@ -3,11 +3,26 @@ function Callback_Startbutton() {
     document.location.href = '/labeling'
 }
 
-function Callback_Startbutton_2() {
-    document.getElementById("StartB").textContent = "CLICK"
-    //document.location.href = '/labeling'
+function Next_Tweet(){
+    document.location.href = '/next_tweet'
 }
 
-function Next_Tweet(){
-    document.getElementById("StartB").textContent = "CLICK"
+function Previous_Tweet(){
+    document.location.href = '/previous_tweet'
+}
+
+function Pro_Label(){
+    document.location.href = '/manual_label_pro'
+}
+
+function Anti_Label(){
+    document.location.href = '/manual_label_anti'
+}
+
+function Neutral_Label(){
+    document.location.href = '/manual_label_neutral'
+}
+
+function News_Label(){
+    document.location.href = '/manual_label_news'
 }
