a7b89c9bc1e6756561d7a4f5e421c0c844237d0e
udjks <udjks@student.kit.edu>
2023-01-16 14:58:13 +0100

    fixed bugs

--- static/js/labeling.js
+++ static/js/labeling.js
@@ -126,6 +126,14 @@ function Switch_To_Model_Training(){
     return false;
 }
 
+function delete_row(){
+    document.location.href = '/delete_row'
+    $.getJSON('/save_results',
+        function(data) {
+        //just call save_results
+    });
+    return false;
+}
 // show descriptive statistics and visualize the dataset, 
 // show further information
 function Switch_To_Model_Analysis(){
@@ -142,15 +150,6 @@ function download_csv() {
     var name = document.getElementById("which_dataset").value;        
     if (name) {
         window.location = '/download_csv?dataset=' + name;
-     }
-     return false;
-}
-
-// delete option
-function delete_csv() {
-    var name = document.getElementById("which_dataset").value;        
-    if (name) {
-        window.location = '/delete_csv?dataset=' + name;
      }
      return false;
 }
