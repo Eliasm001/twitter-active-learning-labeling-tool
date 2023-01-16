c29249cbabd4cedcea5b5c30192e8c10c93a553d
ujohl <molomba19@gmx.at>
2023-01-16 08:21:22 +0100

    updaten von branch Nicolas auf den Stand von branch Elias (z.b. data_management.py oder twitter_api.py) 16.1.

--- src/data_management.py
+++ src/data_management.py
@@ -7,16 +7,19 @@ from nltk import FreqDist
 import matplotlib.pyplot as plt
 
 # all functions and variables specific to the climate change dataset
+
+
 class ClimateChangeData():
 
     # Constructor
     def __init__(self, dataset_name):
-         self.dataset_name = dataset_name
-         # dataset that got chosen
-         self.df_climate = pd.read_csv(f'data/{dataset_name}')
-         # preprocessing steps for the dataset
-         self.df_climate = self.preprocessing(self.df_climate)
-     
+        self.dataset_name = dataset_name
+        # dataset that got chosen
+        self.df_climate = pd.read_csv(f'data/{dataset_name}')
+        # preprocessing steps for the dataset
+        self.df_climate = self.preprocessing(self.df_climate)
+        # tweet counter variable
+        self.tweet_counter_climate = 0
 
     def preprocessing(self, dataset):
         self.dataset = dataset
@@ -25,9 +28,9 @@ class ClimateChangeData():
             self.dataset['my_label'] = np.nan
         # rename the column text to message coming from the twitter api
         if 'text' in self.dataset:
-            self.dataset = self.dataset.rename({'text':'message'},axis=1)
+            self.dataset = self.dataset.rename({'text': 'message'}, axis=1)
         if 'id' in self.dataset:
-            self.dataset = self.dataset.rename({'id':'tweetid'},axis=1)
+            self.dataset = self.dataset.rename({'id': 'tweetid'}, axis=1)
         # when the dataset comes from the api, then we dont have the sentiment attribute
         if 'sentiment' not in self.dataset:
             self.dataset['sentiment'] = np.nan
@@ -37,31 +40,55 @@ class ClimateChangeData():
         # create an index column
         if 'df_index' not in self.dataset:
             self.dataset['df_index'] = self.dataset.index
+        # if we get an empty text then we delete the row
+        #self.dataset = self.dataset.dropna(subset='message')
         # preprocess the hashtags if not done before
-        self.dataset['hashtag'] = self.dataset['message'].apply(lambda x: re.findall(r"#(\w+)", x))
-        print(self.dataset) 
+        self.dataset['hashtag'] = self.dataset['message'].apply(
+            lambda x: re.findall(r"#(\w+)", x)).astype('str')
         return self.dataset
-    
-    # tweet counter variable
-    tweet_counter_climate = 0
 
     # shows the tweets inside of pandas df
     def show_tweets(self):
         tweet = self.df_climate['message'].iloc[self.tweet_counter_climate]
         sentiment = self.df_climate['sentiment'].iloc[self.tweet_counter_climate]
         my_label = self.df_climate['my_label'].iloc[self.tweet_counter_climate]
-        return tweet, sentiment, my_label
-    
+        user_username = self.df_climate['user_username'].iloc[self.tweet_counter_climate]
+        user_name = self.df_climate['user_name'].iloc[self.tweet_counter_climate]
+        created_at = self.df_climate['created_at'].iloc[self.tweet_counter_climate]
+        retweet_count = self.df_climate['retweet_count'].iloc[self.tweet_counter_climate]
+        quote_count = self.df_climate['quote_count'].iloc[self.tweet_counter_climate]
+        like_count = self.df_climate['like_count'].iloc[self.tweet_counter_climate]
+        profile_urls = self.df_climate['profile_urls'].iloc[self.tweet_counter_climate]
+        return tweet, sentiment, my_label, user_username, user_name, created_at, retweet_count, quote_count,\
+            like_count, profile_urls
+
+    # shows the tweets inside of pandas df
+    def show_most_liked_tweets(self):
+        df_most_liked = self.df_climate.iloc[[
+            np.argmax(self.df_climate['like_count'])]].head(1)
+        tweet = df_most_liked['message'].iloc[0]
+        sentiment = df_most_liked['sentiment'].iloc[0]
+        my_label = df_most_liked['my_label'].iloc[0]
+        user_username = df_most_liked['user_username'].iloc[0]
+        user_name = df_most_liked['user_name'].iloc[0]
+        created_at = df_most_liked['created_at'].iloc[0]
+        retweet_count = df_most_liked['retweet_count'].iloc[0]
+        quote_count = df_most_liked['quote_count'].iloc[0]
+        like_count = df_most_liked['like_count'].iloc[0]
+        profile_urls = df_most_liked['profile_urls'].iloc[0]
+        return tweet, sentiment, my_label, user_username, user_name, created_at, retweet_count, quote_count,\
+            like_count, profile_urls
+
     # shows the full dataset inside of pandas df
     def show_full_dataset(self):
-        rows = self.df_climate.iloc[self.tweet_counter_climate:]
+        rows = self.df_climate.iloc[0:]
         rows = list(rows.itertuples(index=False))
         return rows
 
     # puts the users label into the my_label column
     def label(self, label):
-        self.df_climate.loc[self.tweet_counter_climate,'my_label'] = label
-    
+        self.df_climate.loc[self.tweet_counter_climate, 'my_label'] = label
+
     # overwrite the self.dataset with the newly ordered dataset after active learning
     def change_dataset(self, new_dataset):
         self.df_climate = new_dataset
@@ -69,6 +96,11 @@ class ClimateChangeData():
     # overwrite the old dataset with the newly labeled data
     def save_results(self, dataset_name):
         self.df_climate.to_csv(f'data/{dataset_name}', index=False)
+
+    # all labeled values will be put to the end of the dataframe
+    def sort_dataframe(self):
+        self.df_climate = self.df_climate.sort_values(
+            'my_label', na_position='first')
 
     """ 
     create a wordcloud based on the hashtags of the tweets
@@ -79,22 +111,31 @@ class ClimateChangeData():
     Do a Background Calculation for the WordCloud OR
     Calculate it once and then just load it from the plots folder
     """
+
     def create_wordcloud(self):
         # hashtags to list
         hashtags = self.df_climate['hashtag'].tolist()
+        # clean up the hashtag list
+        hashtags_clean = list()
+        for hashtag in hashtags:
+            hashtag = hashtag.replace('[', '')
+            hashtag = hashtag.replace(']', '')
+            hashtag = hashtag.replace("'", '')
+            hashtag = hashtag.replace(" ", '')
+            hashtag = hashtag.lower()
+            hashtag = hashtag.split(',')
+            hashtags_clean.append(hashtag)
         # flatten the list
-        flat_list = [item for sublist in hashtags for item in sublist]    
+        flat_list = [
+            item for sublist in hashtags_clean for item in sublist if item]
         # create a wordcloud to be shown on the analysis page
-        #Frequency of words
+        # Frequency of words
         fdist = FreqDist(flat_list)
         # WordCloud save as a file
         # plot it
-        #WordCloud
-        wc = WordCloud(width=800, height=400, max_words=50).generate_from_frequencies(fdist)
-        # save the wordcloud 
+        # WordCloud
+        wc = WordCloud(width=800, height=400,
+                       max_words=50).generate_from_frequencies(fdist)
+        # save the wordcloud
         image = wc.to_image()
-        image.save('./static/plots/wordcloud.png')   
-       
-        
-        
-
+        image.save('./static/plots/wordcloud.png')
--- src/twitter_api.py
+++ src/twitter_api.py
@@ -11,30 +11,107 @@ import datetime
 """ 
 class API:
     def __init__(self, search_term, language, start, end, max_results):
-        # Suchbegriff für den Tweet
-        self.language = str(language)           #Gerade en oder de
-        self.start = datetime.datetime.strptime(start, '%Y-%m-%d')  #Format Muss passend sein zu dem was aus dem Frontend kommt
-        self.end = datetime.datetime.strptime(end, '%Y-%m-%d')      #Format Muss passend sein zu dem was aus dem Frontend kommt
-        self.max_results = int(max_results)     # Limit von 100 (kann auch umgangen werden, muss noch hinzugefügt werden)
+
+
+        '''
+        Receive the data from the Frontend and save into the search instance. 
+        Then build a search query for the API
+        '''
+        self.language = str(language)
+        self.start = datetime.datetime.strptime(start, '%Y-%m-%d')
+        self.end = datetime.datetime.strptime(end, '%Y-%m-%d')   
+        self.max_results = int(int(max_results)/10)
+        self.search_term = search_term
+        #Create a query
+        query = self.search_term +' lang:' + self.language + ' -is:retweet'
+
+
+
+        '''
+        Get the date, from when the API request was made
+        '''
         # Create a datetime instance
         datetimeInstance = datetime.datetime.today()       
         # Get only the date instance from datetime instance
         self.creation = datetimeInstance.date() # Könnte man zum file Name hinzufügen
-        self.search_term = search_term
-        #Create a query
-        query = self.search_term +' lang:' + self.language
         #Create a client and a search request for the API
-        self.client = tweepy.Client(bearer_token = Bearer_Token)
-        self.response = self.client.search_recent_tweets(query, start_time=self.start, end_time=self.end, max_results=self.max_results)
+
+        '''
+        Create an instance of a client to access the data through the Twitter API and get the data (Tweetfields) and the special fields (Userfields)
+        '''
+        client = tweepy.Client(bearer_token= Bearer_Token)
+
+        data = list()
+        includes = list()
+
+        Paginator = tweepy.Paginator(client.search_recent_tweets,
+                                    query=query, 
+                                    start_time=self.start,
+                                    end_time=self.end,
+                                    max_results=10, 
+                                    expansions='author_id',
+                                    tweet_fields=['created_at', 'public_metrics'],
+                                    user_fields=['username', 'profile_image_url'],
+                                    limit = self.max_results
+                                    )
+
+        for response in Paginator:
+            data.append(response.data)
+            includes.append(response.includes)
+
+
+
+    
+        '''
+        Extract the Twitter User data from the response of the client
+        '''
+        users = list()
+
+        for i, page in enumerate(Paginator):
+            users.append(includes[i]['users'])
+
+        self.users = [item for sublist in users for item in sublist]
+
+
+        self.profile_urls = list()
+        self.user_username = list()
+        self.user_name = list()
+        self.user_ids = list()
+        for user in self.users:
+            self.profile_urls.append(user.profile_image_url)
+            self.user_username.append(user.username)
+            self.user_name.append(user.name)
+            self.user_ids.append(user.id)
+        # df with the user data (only unique users)
+        self.df_users = pd.DataFrame({'profile_urls':self.profile_urls, 'user_username':self.user_username,\
+                                 'user_name':self.user_name, 'user_ids':self.user_ids})
+
+
+
+
+        '''
+        Extract the Twitter User data from the response of the client
+        '''
+        self.data = [item for sublist in data for item in sublist]
         #Create a dataframe of the Response with column of interest 'text' 
-        self.df_api = pd.DataFrame(self.response.data)
-        #tweet = df_api['text'].iloc[self.tweet_counter_api]
-        # sollte auch so funktionieren, da in data_management definiert
-        # self.df_api["my_label"] = np.nan
-        # self.df_api["sentiment"] = np.nan
+        self.df_api = pd.DataFrame(self.data)
+        # format the public information feature and add the new columns
+
+
+
+
+        '''
+        Merge The Response into a single DataFrame
+        '''
+        self.df_api_merged = self.df_api.merge(self.df_api['public_metrics'].apply(pd.Series),left_index=True, right_index=True)\
+            .drop('public_metrics',axis=1)
+        # combine the tweet df with the user df
+        self.df_api_users_merge = self.df_api_merged.merge(self.df_users, left_on='author_id', right_on='user_ids')
+        
+
  
     tweet_counter_api = 0
     
     # shows the tweets inside of pandas df
     def save_dataset(self):
-        self.df_api.to_csv(f'data/{self.search_term}_{self.language}_{self.max_results}_{self.creation}.csv')
+        self.df_api_users_merge.to_csv(f'data/{self.search_term}_{self.language}_{self.max_results}_{self.creation}.csv')
